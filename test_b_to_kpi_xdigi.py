1edf21a2033ab3cdc779011adf7f3f690f619a51
Emily Jiang <ejiang@umd.edu>
2022-01-31 20:11:50 +0100

    running over all MC files remotely

--- test_b_to_kpi_xdigi.py
+++ test_b_to_kpi_xdigi.py
@@ -6,17 +6,22 @@ from RecoConf.global_tools import stateProvider_with_simplified_geom
 from RecoConf.reconstruction_objects import upfront_reconstruction, reconstruction
 from RecoConf.hlt1_tracking import default_ft_decoding_version
 
+from Gaudi.Configuration import FileCatalog
+from GaudiConf import IOHelper
+
 #set decoding version
 ft_decoding_version = 6
 default_ft_decoding_version.global_bind(value=ft_decoding_version)
 
 public_tools = [stateProvider_with_simplified_geom()]
 
-input_files = ['root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/Upgrade/XDIGI/00127044/0000/00127044_00000056_1.xdigi']
-
+input_files = ['LFN:/lhcb/MC/Upgrade/XDIGI/00127044/0000/00127044_00000056_1.xdigi',]
+#input_files = ['root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/Upgrade/XDIGI/00127044/0000/00127044_00000056_1.xdigi']
 options.input_files = input_files
 options.input_type = "ROOT"
 options.input_raw_format = 0.3
+
+FileCatalog().Catalogs = ["xmlcatalog_file:/afs/cern.ch/work/e/ejiang/public/stack/myCatalogDown.xml"]
     
 #options.evt_max = 100                                                                       
 options.dddb_tag = 'dddb-20201211' 
